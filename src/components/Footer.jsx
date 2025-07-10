cea0d4430cb64ea5509d16eb9af028a677bc3608
Kousik-Bytebandits <kousik.bbtech@gmail.com>
2025-07-10 21:07:04 +0530

    order tracking and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,75 +2,97 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#111111] text-white px-6 py-12 font-sans">
-      <div className="max-w-[75%] mx-auto space-y-10">
+    <footer className="bg-[#111111] text-white px-4 md:px-6 py-12 font-sans">
+      <div className="max-w-[90%] xxxl:max-w-[80%] mx-auto space-y-10">
 
         {/* Top Section */}
-        <div className="flex flex-col lg:flex-row justify-between  gap-10">
+        <div className="flex flex-col laptop:flex-row justify-between laptop:items-start gap-10">
 
           {/* Logo & Address */}
-          <div className="lg:w-1/3 text-center lg:text-left space-y-4 lg:mb-16">
+          <div className="text-center laptop:text-left space-y-4 laptop:w-[320px]">
             <img
               src="images/zia_footer.png"
               alt="Zia Logo"
-              className="mx-auto lg:mx-0 w-[140px] laptop:w-[140px] hd:w-[180px] xxxl:w-[200px]"
+              className="mx-auto laptop:mx-0 w-[140px] laptop:w-[140px] hd:w-[180px] xxxl:w-[200px]"
             />
-            <p className="text-[15px] laptop:text-[14px] hd:text-[15px] xxxl:text-[18px] leading-relaxed lg:leading-7 tracking-wide whitespace-pre-line">
-              {`ZIA HERBAL PRO PVT LTD.,
-ISO 9001:2015 Certified Company
-No. S-9, I Floor,
-Thiru Vi Ka Industrial Estate,
-Guindy, Chennai - 600 032., Tamilnadu`}
+            <p className="text-[15px] laptop:text-[14px] hd:text-[15px] xxxl:text-[17px] leading-relaxed tracking-wide whitespace-pre-line font-sans">
+              {`ZIA HERBAL PRO PVT LTD.,\nISO 9001:2015 Certified Company\nNo. S-9, I Floor,\nThiru Vi Ka Industrial Estate,\nGuindy, Chennai – 600 032., Tamilnadu`}
             </p>
+            <p className="font-semibold text-[14px] tracking-wider mt-2">GSTIN: 33AABCZ9392A1ZA</p>
           </div>
 
-          {/* Links */}
-          <div className="flex flex-row sm:flex-row justify-around gap-10 lg:gap-40 text-center lg:text-left">
-
+          {/* Link Sections */}
+          <div className="flex flex-wrap  justify-between lg:w-[65%] lg:mt-14 font-archivo">
             {/* Useful Links */}
-            <div className="lg:mt-8 space-y-4">
-              <h3 className="font-archivo text-[18px] laptop:text-[19px] hd:text-[20px] xxxl:text-[21px] text-left">Useful links</h3>
-              <ul className="space-y-3 text-[15px] laptop:text-[16px] hd:text-[17px] xxxl:text-[18px] font-archivo text-left">
-                <li><Link to="/shoplist" className="hover:underline hover:text-[#00FF94]">Skin Care</Link></li>
-                <li><Link to="/shoplist" className="hover:underline hover:text-[#00FF94]">Hair Care</Link></li>
-                <li><Link to="/ordertracking" className="hover:underline hover:text-[#00FF94]">Track Order</Link></li>
+            <div className="mt-6 space-y-3 min-w-[110px]">
+              <h3 className="text-[20px] laptop:text-[17px] hd:text-[18px] xxxl:text-[20px] font-semibold mb-2">Useful links</h3>
+              <ul className="space-y-2 text-[16px] text-white tracking-wider">
+                <li><Link to="/shoplist" className="hover:underline">Hand Wash</Link></li>
+                <li><Link to="/shoplist" className="hover:underline">Soap</Link></li>
+                <li><Link to="/shoplist" className="hover:underline">Oils</Link></li>
+                <li><Link to="/shoplist" className="hover:underline">Shampoo</Link></li>
+                <li><Link to="/shoplist" className="hover:underline">Serums</Link></li>
+              </ul>
+            </div>
+
+            {/* Customer Care */}
+            <div className="mt-6 space-y-3 min-w-[150px]">
+              <h3 className="text-[20px] laptop:text-[17px] hd:text-[18px] xxxl:text-[20px] font-semibold mb-2">Customer Care</h3>
+              <ul className="space-y-2 text-[16px] text-white">
+                <li><Link to="/contactus" className="hover:underline">Contact Us</Link></li>
+                <li><Link to="/ordertracking" className="hover:underline">Track Order</Link></li>
+                <li className="flex items-center gap-2">
+                  <img src="icons/whatsapp.png" className="w-[16px] h-[16px] -ml-6" alt="whatsapp" />
+                  +91 - 8939843483
+                </li>
+                <li className="flex items-center gap-2 ">
+                  <img src="icons/mail.png" className="w-[20px] h-[18px] -ml-6" alt="email" />
+                  Ziaherbalpro@gmail.com
+                </li>
+              </ul>
+            </div>
+
+            {/* Policies */}
+            <div className="space-y-3 mt-6 min-w-[150px]">
+              <h3 className="text-[20px] laptop:text-[17px] hd:text-[18px] xxxl:text-[20px] font-semibold mb-2">Policies</h3>
+              <ul className="space-y-2 text-[16px] text-white">
+                <li>Privacy Policy</li>
+                <li>Return & Refund Policy</li>
+                <li>Shipping Policy</li>
+                <li>Payment Policy</li>
+                <li>Cookie Policy</li>
               </ul>
             </div>
 
             {/* About */}
-            <div className="lg:mt-8 space-y-4">
-              <h3 className="font-archivo text-[18px] laptop:text-[19px] hd:text-[20px] xxxl:text-[21px] text-left">About</h3>
-              <ul className="space-y-3 text-[15px] laptop:text-[16px] hd:text-[17px] xxxl:text-[18px] text-left">
-                <li><Link to="/about" className="hover:underline hover:text-[#00FF94]">About Us</Link></li>
-                <li><Link to="/contactus" className="hover:underline hover:text-[#00FF94]">Contact Us</Link></li>
+            <div className="space-y-3 mt-6 min-w-[130px]">
+              <h3 className="text-[20px] laptop:text-[17px] hd:text-[18px] xxxl:text-[20px] font-semibold mb-2">About</h3>
+              <ul className="space-y-2 text-[16px] text-white">
+                <li>Our Story</li>
+                <li>Our Mission</li>
+                <li>Our Vision</li>
+                <li>Ingredients</li>
               </ul>
             </div>
-          </div>
 
-          {/* Contact */}
-          <div className="lg:mt-8 font-archivo text-center lg:text-left space-y-4 order-last lg:order-none">
-            <h3 className="text-[18px] laptop:text-[19px] hd:text-[20px] xxxl:text-[21px]">Contact Us</h3>
-            <div className="flex items-center justify-center lg:justify-start gap-2 text-[15px] laptop:text-[16px]">
-              <img src="icons/whatsapp.png" className="w-5 h-5" alt="whatsapp" />
-              <span>+91 - 8939843483</span>
-            </div>
-            <div className="flex items-center justify-center lg:justify-start gap-2 text-[15px] laptop:text-[16px]">
-              <img src="icons/mail.png" className="w-6" alt="email" />
-              <span>Ziaherbalpro@gmail.com</span>
+            {/* Info */}
+            <div className="mt-6 space-y-3 min-w-[130px]">
+              <h3 className="text-[20px] laptop:text-[17px] hd:text-[18px] xxxl:text-[20px] font-semibold mb-2">Info</h3>
+              <ul className="space-y-2 text-[16px]  text-white">
+                <li>Terms & Conditions</li>
+                <li>Medical Disclaimer</li>
+              </ul>
             </div>
           </div>
         </div>
 
-        {/* Divider */}
-        <hr className="border-gray-200 lg:hidden" />
+        {/* Divider Line */}
+        <div className="border-t border-gray-400 w-full my-2" />
 
-        {/* Bottom */}
-        <div className="flex flex-col lg:flex-row justify-between items-center text-gray-400 text-[14px] laptop:text-[12px] hd:text-[14px] xxxl:text-[17px] tracking-wide mt-6 space-y-4 lg:space-y-0 text-center">
-          <p>© 2025. Zia. All rights reserved.</p>
-          <div className="flex flex-col gap-2 lg:flex-row lg:gap-20 text-white l">
-            <p className="hover:text-[#00FF94] cursor-pointer">Terms & Conditions</p>
-            <p className="hover:text-[#00FF94] cursor-pointer">Privacy Policy</p>
-          </div>
+        {/* Footer Bottom */}
+        <div className="flex flex-col laptop:flex-row justify-between items-center text-[15px] text-gray-400">
+          <p>Copyright © 2025. Zia. All rights reserved.</p>
+          <p className="mt-2 laptop:mt-0">Developed by ByteBandits.in</p>
         </div>
       </div>
     </footer>
--- src/pages/OrderTracking.jsx
+++ src/pages/OrderTracking.jsx
@@ -1,62 +1,156 @@
-import  { useState } from "react";
 import Footer from "../components/Footer";
 
-export default function OrderTracking() {
-  const [shipmentId, setShipmentId] = useState("");
-  const [email, setEmail] = useState("");
+const TrackingStep = ({ icon, title, description, isCompleted }) => {
+  return (
+    <div className="relative flex items-start space-x-3 z-10 laptop:hidden">
+      <div className="z-20">
+        <div className={`w-3 h-3 rounded-full ${isCompleted ? "bg-[#388E3C]" : "bg-gray-300"} mt-[16px]`} />
+      </div>
+      <div className="flex space-x-2 font-archivo">
+        <img src={icon} alt={title} className="w-[40px] h-[40px] mt-[1px]" />
+        <div>
+          <p className={`font-semibold text-[18px] ${isCompleted ? "text-[#4A4A4A]" : "text-gray-400"}`}>{title}</p>
+          <p className="text-[14px] text-[#B6B6B6]">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-  };
+const StepTimeline = ({ steps }) => (
+  <>
+    <div className="relative ml-2 laptop:hidden">
+      <div className="absolute left-[5.5px] top-[20px] bottom-[42px] w-[1px] bg-[#388E3C] z-0" />
+      <div className="flex flex-col space-y-10 mt-10 mb-4">
+        {steps.map((step) => (
+          <TrackingStep
+            key={step.title}
+            icon={step.icon}
+            title={step.title}
+            description={step.description}
+            isCompleted={step.status === "completed" || step.status === "current"}
+          />
+        ))}
+      </div>
+    </div>
+
+    <div className="hidden laptop:block">
+      <div className="flex justify-between w-full px-2 hd:px-6 xxxl:px-12 mt-8">
+        {steps.map((step) => (
+          <div key={step.title} className="flex flex-col items-center text-center">
+            <div className=" p-2 rounded-full">
+              <img src={step.icon} alt={step.title} className="w-full h-14" />
+            </div>
+            <p className="mt-2 font-semibold xxxl:text-[24px] laptop:text-[18px] hd:text-[20px] text-[#4A4A4A]">{step.title}</p>
+            <p className="xxxl:text-[20px] laptop:text-[14px] hd:text-[16px] text-[#B6B6B6] max-w-[280px]">{step.description}</p>
+          </div>
+        ))}
+      </div>
+      <div className="relative mt-6 px-2 hd:px-6 xxxl:px-44 hd:px-40 laptop:px-36 mt-8 mb-10">
+  {/* Line connecting all steps */}
+  <div className="absolute top-3  xxxl:left-44 xxxl:right-44 laptop:left-36 laptop:right-36 hd:left-40 hd:right-40 h-[1px] bg-[#388E3C] z-0 " />
+
+  {/* Green Dots */}
+  <div className="flex justify-between mx-auto items-center relative z-10">
+    {steps.map((_, idx) => (
+      <div key={idx} className="w-6 h-6 bg-[#388E3C] rounded-full" />
+    ))}
+  </div>
+</div>
+
+    </div>
+  </>
+);
+
+const OrderTrackingCard = ({ orderId, productName, volume, price, image, steps }) => (
+  <div className="border rounded-xl p-4 text-[#2B452C] shadow-around-soft bg-white space-y-2">
+    {/* Desktop Layout */}
+    <div className="hidden lg:flex  lg:justify-between lg:items-center lg:gap-4">
+      <img
+        src={image}
+        alt={productName}
+        className="xxxl:w-[160px] xxxl:h-[270px] object-contain"
+      />
+      <div className="flex-1 space-y-2">
+        <p className="xxxl:text-[29px] laptop:text-[20px] hd:text-[26px] font-archivo font-semibold text-[#333333] uppercase">{productName}</p>
+        <p className="xxxl:text-[23px] laptop:text-[16px] hd:text-[20px] text-[#AEAEAE]">{volume}</p>
+        <p className="xxxl:text-[40px] laptop:text-[28px] hd:text-[32px] font-bold text-[#151515] mt-2">₹{price}</p>
+      </div>
+      <div className="mr-10 -mt-[8%]">
+      <p className="xxxl:text-[30px] laptop:text-[20px] hd:text-[24px]  text-[#2B452C] font-medium whitespace-nowrap">Order Id : {orderId}</p>
+      </div>
+    </div>
+
+    {/* Mobile Layout */}
+    <div className="laptop:hidden">
+      <p className="text-[14px] text-[#2B452C] font-medium mb-1">Order Id : {orderId}</p>
+      <div className="flex justify-between items-start">
+        <div>
+          <p className="text-[16px] font-archivo font-semibold text-[#333333] uppercase">{productName}</p>
+          <p className="text-[14px] text-[#AEAEAE]">{volume}</p>
+          <p className="text-[28px] font-bold text-[#151515] mt-2">₹{price}</p>
+        </div>
+        <img
+          src={image}
+          alt={productName}
+          className="w-[94px] h-[125px] object-contain -mt-8 mb-4"
+        />
+      </div>
+    </div>
+
+    <div className="border-t border-[#B2BA98] my-2 mx-10" />
+    <StepTimeline steps={steps} />
+  </div>
+);
+
+export default function OrderTracking() {
+  const stepsCompleted = [
+    {
+      title: "Shipped",
+      description: "Your order has been handed over to our delivery partner and is on its way.",
+      icon: "/icons/shipped.png",
+      status: "completed",
+    },
+    {
+      title: "Out of delivery",
+      description: "Your package is arriving today. Please keep your phone nearby.",
+      icon: "/icons/ood.png",
+      status: "completed",
+    },
+    {
+      title: "Delivered",
+      description: "Your package was successfully delivered. We hope you love it!",
+      icon: "/icons/delivered.png",
+      status: "completed",
+    },
+  ];
 
   return (
     <>
-    <div className="h-[75vh] pt-20 lg:h-[90vh] flex flex-col justify-center items-center bg-white px-4 text-center">
-      {/* Header */}
-      <p className=" font-tenor mb-2 text-[40px] lg:hidden">Order Tracking</p>
-      <p className="font-archivo text-[20px] text-[#676A5E] hidden lg:block tracking-wide mb-10">Track Your Shipment</p>
-      <h1 className="text-[16px] lg:text-[34px] font-tenor lg:tracking-[0.2em] tracking-[0.1em] text-[#676A5E] uppercase mb-12">
-        Need to see your shipment?
-      </h1>
-
-      {/* Form */}
-      <form onSubmit={handleSubmit} className="w-full max-w-md  lg:max-w-2xl space-y-10">
-        {/* Shipment ID */}
-        <div className="relative">
-          <input
-            type="text"
-            value={shipmentId}
-            onChange={(e) => setShipmentId(e.target.value)}
-            placeholder="Enter Shipment ID no"
-            className="w-full text-center text-[28px] placeholder-[#B2BA98] font-archivo border-b-2 border-[#676A5E] focus:outline-none pb-6"
+      <div className="min-h-screen pt-24 bg-white px-4 py-6 xxxl:px-60 xxxl:py-20 hd:px-36 laptop:px-36 font-archivo mx-auto">
+        <h1 className="text-center text-[22px] font-tenor text-[#2E3A27] mb-5 hd:text-[40px] laptop:text-[36px] xxxl:text-[50px]">
+          Track Order
+        </h1>
+        <div className="space-y-6">
+          <OrderTrackingCard
+            orderId="887745"
+            productName="ZIA HERBAL HAIR SHAMPOO"
+            volume="100 ML"
+            price="400"
+            image="/images/h_shampoo.png"
+            steps={stepsCompleted}
           />
-          <p className="text-[14px] lg:text-[18px] font-archivo text-[#676A5E] mt-2 ">
-            For multiple queries use commas (,) eg: 79034111122, 79034111041
-          </p>
-        </div>
-
-        {/* Email */}
-        <div className="relative ">
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email ID"
-            className="w-full text-center text-[28px] placeholder-[#B2BA98] font-archivo border-b-2 lg:mt-12 border-[#676A5E] focus:outline-none pb-6"
+          <OrderTrackingCard
+            orderId="887745"
+            productName="ZIA HERBAL HAIR SHAMPOO"
+            volume="100 ML"
+            price="400"
+            image="/images/h_shampoo.png"
+            steps={stepsCompleted}
           />
         </div>
-
-        {/* Submit Button */}
-        <button
-          type="submit"
-          className="bg-black text-white text-[16px] font-tenor px-7 py-4 rounded-full flex items-center justify-center mx-auto"
-        >
-          Submit Now <span className="ml-2 text-lg">→</span>
-        </button>
-      </form>
-    </div>
-    <Footer/>
+      </div>
+      <Footer />
     </>
   );
 }
